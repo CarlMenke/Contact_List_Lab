425e2ac0d2139282d12b85f4565309b10c4278e2
carlmenke <carlmenke13@gmail.com>
2022-08-25 14:25:42 -0700

    added styling

--- contact-list-app/src/components/DetailedContact.js
+++ contact-list-app/src/components/DetailedContact.js
@@ -6,7 +6,7 @@ const DetailedContact = (props) => {
     return (
         <div id ='detailed-contact' key = {props.key}>
             <img  src = {props.picture} alt = 'profle pic'></img>
-            <div id = 'Personal information'>
+            <div id = 'personal-information'>
                 <div>Name: {props.name}</div>
                 <div>Cell: {props.mobile}</div>
                 <div>Phone: {props.phone}</div>
